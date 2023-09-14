04cadfe650b9e6dec680e266af9949a563bfe858
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-09-14 14:42:25 +0200

    changed login route to /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   root 'pages#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
